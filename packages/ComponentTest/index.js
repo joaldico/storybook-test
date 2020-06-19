bdd8d927135560e741f4e4559729bb5d48d2d838
Josue <j.diaz@bucares.com>
2020-06-18 20:49:39 -0400

    -update lerna

--- packages/ComponentTest/index.js
+++ packages/ComponentTest/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import './index.css';
 class Button extends React.PureComponent {
   render() {
-    const { disabled, size, text } = this.props;
+    const { disabled, size } = this.props;
     return (
       <button className={`button button--${size}`} disabled={disabled}>
-        {text}
+        {"Este es mi componente de test"}
       </button>
     );
   }
